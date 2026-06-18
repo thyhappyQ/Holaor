d22b7fda5fa60dfc7f09a7e003ef227f471dd455
thyhappy <busilpio@outlook.com>
2026-06-18 16:23:29 +0800

    add a function to verify whether the length of the ID card is correct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+const STANDARD_ID_CARD_LENGTH:u8 = 18;
+
+fn verify_length(source:&String)->Option<String>{
+    // Turn the source string to the chars
+    let chars = source.chars();
+
+    // Get the length of the ID card
+    let length = chars.count();
+
+    // Check if the length of the ID card is correct
+    if STANDARD_ID_CARD_LENGTH != length as u8 {
+        return Some("The length of this ID card is ".to_string()
+                + length.to_string().as_str()
+                + ",but we except "
+                + STANDARD_ID_CARD_LENGTH.to_string().as_str());
+    }
+
+    // If everything is OK,we return "None" to show that there is not any error
+    None
+}
